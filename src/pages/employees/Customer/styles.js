418143e3c53ebe386978f05e66b5e0ead5fd5f11
Elton Rodrigues <erodrigues.dev@gmail.com>
2020-05-10 18:59:26 -0300

    feature: customers - crud

---
+++ src/pages/employees/Customer/styles.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const FilterContainer = styled.div`
+  margin-top: 24px;
+`;
--- src/pages/employees/index.js
+++ src/pages/employees/index.js
@@ -1,3 +1,5 @@
 export { default as Home } from './Home';
 export { default as Employee } from './Employee';
 export { default as EmployeeForm } from './Employee/Form';
+export { default as Customer } from './Customer';
+export { default as CustomerForm } from './Customer/Form';
