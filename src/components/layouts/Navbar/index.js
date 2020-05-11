9bb637e1c8dc059cdde1c4c2c44625c24ae755de
Elton Rodrigues <erodrigues.dev@gmail.com>
2020-05-11 18:53:47 -0300

    add link to home in logo

--- src/components/layouts/Navbar/index.js
+++ src/components/layouts/Navbar/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { FiMenu } from 'react-icons/fi';
+import Link from 'react-router-dom/Link';
 
 import { Container, Buttons, Button } from './styles';
 
 const Navbar = ({ sidebarOpen, toggleSidebar }) => (
   <Container>
-    <div>{!sidebarOpen && <img src="/images/logo-1.png" alt="logo" />}</div>
+    <div>
+      {!sidebarOpen && (
+        <Link to="/">
+          <img src="/images/logo-1.png" alt="logo" />
+        </Link>
+      )}
+    </div>
     <Buttons>
       <Button onClick={toggleSidebar}>
         <FiMenu size={22} />
