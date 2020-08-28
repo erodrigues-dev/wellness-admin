8e87394b46c3864bbae6bf150edf55c6999d0b84
Elton Rodrigues <erodrigues.dev@gmail.com>
2020-08-27 23:12:29 -0300

    create input datepicker component

---
+++ src/components/InputDatePicker/index.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { FormControl, InputGroup } from 'react-bootstrap';
+import { FiCalendar, FiXCircle } from 'react-icons/fi';
+
+import { formatToDisplay } from '~/helpers/date';
+
+import DatePicker from '../DatePicker';
+import { Container } from './styles';
+
+function InputDatePicker({ min, max, value, onChange, ...props }) {
+  const [open, setOpen] = useState(false);
+  const [date, setDate] = useState(null);
+  const [formated, setFormated] = useState('');
+
+  useEffect(() => {
+    setDate(value);
+  }, [value]);
+
+  useEffect(() => {
+    setFormated(date instanceof Date ? formatToDisplay(date) : '');
+  }, [date]);
+
+  function handleChangeDate(selectedDate) {
+    setOpen(false);
+    setDate(selectedDate);
+    onChange({
+      target: { name: props.name, value: selectedDate },
+    });
+  }
+
+  function handleClear(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    handleChangeDate(null);
+  }
+
+  return (
+    <Container onClick={() => setOpen(true)}>
+      <InputGroup>
+        <FormControl
+          {...props}
+          value={formated}
+          onChange={() => {}}
+          placeholder="mm/dd/yyyy"
+          autoComplete="off"
+        />
+        <InputGroup.Append>
+          {date && (
+            <InputGroup.Text onClick={handleClear}>
+              <FiXCircle />
+            </InputGroup.Text>
+          )}
+          <InputGroup.Text>
+            <FiCalendar />
+          </InputGroup.Text>
+        </InputGroup.Append>
+      </InputGroup>
+      {open && (
+        <DatePicker
+          onChange={handleChangeDate}
+          onClose={() => setOpen(false)}
+          minDate={min}
+          maxDate={max}
+          value={date}
+        />
+      )}
+    </Container>
+  );
+}
+
+export default InputDatePicker;
