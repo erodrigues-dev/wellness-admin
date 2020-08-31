124079076cb6b548723a1dce581c7b6c4c5684ce
Elton Rodrigues <erodrigues.dev@gmail.com>
2020-08-31 16:36:28 -0300

    add consts for schedule form

---
+++ src/pages/employees/Activity/Schedule/Form/consts.js
@@ -0,0 +1,21 @@
+export const WEEKDAYS = [
+  { name: 'Sunday', value: 'sunday' },
+  { name: 'Monday', value: 'monday' },
+  { name: 'Tuesday', value: 'tuesday' },
+  { name: 'Wednesday', value: 'wednesday' },
+  { name: 'Thursday', value: 'thursday' },
+  { name: 'Friday', value: 'friday' },
+  { name: 'Saturday', value: 'saturday' },
+];
+
+export const RECURRENCE = [
+  { name: 'Day(s)', value: 'daily' },
+  { name: 'Week(s)', value: 'weekly' },
+  { name: 'Month(s)', value: 'monthly' },
+];
+
+export const RECURRENCE_ENDSIN = [
+  { name: 'Never', value: 'never' },
+  { name: 'In', value: 'in' },
+  { name: 'After', value: 'after' },
+];
