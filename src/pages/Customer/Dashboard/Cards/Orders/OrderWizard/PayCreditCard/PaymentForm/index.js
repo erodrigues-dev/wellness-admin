ba9bf673aa1a487f5ee6f88488e44ce10fa78aa4
Luan Panno <luanpanno@gmail.com>
2020-10-30 20:28:48 -0300

    square payments test

--- src/pages/Customer/Dashboard/Cards/Orders/OrderWizard/PayCreditCard/PaymentForm/index.js
+++ src/pages/Customer/Dashboard/Cards/Orders/OrderWizard/PayCreditCard/PaymentForm/index.js
@@ -1,5 +1,6 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React, { useEffect, useState } from 'react';
-import { Col, Form, Row } from 'react-bootstrap';
+// import { Button, Col, Form, Row } from 'react-bootstrap';
 
 import { Container } from './styles';
 
@@ -8,104 +9,156 @@ const PaymentForm = ({ SqPaymentForm }) => {
     cardBrand: '',
     nonce: undefined,
   });
-
-  useEffect(() => {
-    const config = {
-      applicationId: process.env.REACT_APP_SQUARE_APP_ID,
-      locationId: process.env.REACT_APP_SQUARE_LOCATION_ID,
-      token: process.env.REACT_APP_SQUARE_TOKEN,
-      inputClass: 'sq-input',
-      autoBuild: false,
-      cardNumber: {
-        elementId: 'sq-card-number',
-      },
-      cvv: {
-        elementId: 'sq-cvv',
-      },
-      expirationDate: {
-        elementId: 'sq-expiration-date',
-      },
-      postalCode: {
-        elementId: 'sq-postal-code',
-      },
-      callbacks: {
-        createPaymentRequest: () => {
-          return {
-            requestShippingAddress: false,
-            requestBillingInfo: true,
-            currencyCode: 'USD',
-            countryCode: 'US',
-            total: {
-              label: 'MERCHANT NAME',
+  const config = {
+    applicationId: process.env.REACT_APP_SQUARE_APP_ID,
+    locationId: process.env.REACT_APP_SQUARE_LOCATION_ID,
+    inputClass: 'sq-input',
+    autoBuild: false,
+    cardNumber: {
+      elementId: 'sq-card-number',
+      placeholder: 'ex: xxxx xxxx xxxx xxxx',
+    },
+    cvv: {
+      elementId: 'sq-cvv',
+      placeholder: 'ex: 000',
+    },
+    expirationDate: {
+      elementId: 'sq-expiration-date',
+      placeholder: 'ex: 12/21',
+    },
+    postalCode: {
+      elementId: 'sq-postal-code',
+      placeholder: 'ex: 1111111',
+    },
+    callbacks: {
+      createPaymentRequest: () => {
+        return {
+          requestShippingAddress: false,
+          requestBillingInfo: true,
+          currencyCode: 'USD',
+          countryCode: 'US',
+          total: {
+            label: 'MERCHANT NAME',
+            amount: '100',
+            pending: false,
+          },
+          lineItems: [
+            {
+              label: 'Subtotal',
               amount: '100',
               pending: false,
             },
-            lineItems: [
-              {
-                label: 'Subtotal',
-                amount: '100',
-                pending: false,
-              },
-            ],
-          };
-        },
-        cardNonceResponseReceived: (errors, nonce) => {
-          if (errors) {
-            // Log errors from nonce generation to the Javascript console
-            console.log('Encountered errors:');
-            errors.forEach((error) => {
-              console.log(`  ${error.message}`);
-            });
-
-            return;
-          }
-          setPaymentInfo({
-            nonce,
-          });
-        },
-        unsupportedBrowserDetected: () => {},
-        inputEventReceived: (inputEvent) => {
-          switch (inputEvent.eventType) {
-            case 'focusClassAdded':
-              break;
-            case 'focusClassRemoved':
-              break;
-            case 'errorClassAdded':
-              document.getElementById('error').innerHTML =
-                'Please fix card information errors before continuing.';
-              break;
-            case 'errorClassRemoved':
-              document.getElementById('error').style.display = 'none';
-              break;
-            case 'cardBrandChanged':
-              if (inputEvent.cardBrand !== 'unknown') {
-                setPaymentInfo({
-                  cardBrand: inputEvent.cardBrand,
-                });
-              } else {
-                setPaymentInfo({
-                  cardBrand: '',
-                });
-              }
-              break;
-            case 'postalCodeChanged':
-              break;
-            default:
-              break;
-          }
-        },
-        paymentFormLoaded: () => {
-          document.getElementById('name').style.display = 'inline-flex';
-        },
+          ],
+        };
       },
-    };
-    const paymentForm = new SqPaymentForm(config);
+      cardNonceResponseReceived: (errors, nonce) => {
+        if (errors) {
+          // Log errors from nonce generation to the Javascript console
+          console.log('Encountered errors:');
+          errors.forEach((error) => {
+            console.log(`  ${error.message}`);
+          });
+
+          return;
+        }
+        setPaymentInfo({
+          nonce,
+        });
+      },
+      unsupportedBrowserDetected: () => {},
+      inputEventReceived: (inputEvent) => {
+        switch (inputEvent.eventType) {
+          case 'focusClassAdded':
+            break;
+          case 'focusClassRemoved':
+            break;
+          case 'errorClassAdded':
+            document.getElementById('error').innerHTML =
+              'Please fix card information errors before continuing.';
+            break;
+          case 'errorClassRemoved':
+            document.getElementById('error').style.display = 'none';
+            break;
+          case 'cardBrandChanged':
+            if (inputEvent.cardBrand !== 'unknown') {
+              setPaymentInfo({
+                cardBrand: inputEvent.cardBrand,
+              });
+            } else {
+              setPaymentInfo({
+                cardBrand: '',
+              });
+            }
+            break;
+          case 'postalCodeChanged':
+            break;
+          default:
+            break;
+        }
+      },
+      paymentFormLoaded: () => {
+        document.getElementById('name').style.display = 'inline-flex';
+      },
+    },
+  };
+  const paymentForm = new SqPaymentForm(config);
+
+  function formBuild() {
     paymentForm.build();
-  }, [SqPaymentForm]);
+  }
+
+  useEffect(() => {
+    formBuild();
+
+    // return () => {
+    //   paymentForm.destroy();
+    // };
+  }, []);
 
   return (
     <Container>
-      <Form id="form-container">
+      <div className="container">
+        <div id="form-container">
+          <div id="sq-walletbox">
+            <button type="button" className="wallet-button" id="sq-apple-pay" />
+            <button
+              type="button"
+              className="wallet-button"
+              id="sq-masterpass"
+            />
+            <button
+              type="button"
+              className="wallet-button"
+              id="sq-google-pay"
+            />
+            <hr />
+          </div>
+
+          <div id="sq-ccbox">
+            <p>
+              <span>Enter Card Info Below </span>
+              <span>{paymentInfo.cardBrand.toUpperCase()}</span>
+            </p>
+            <div id="cc-field-wrapper">
+              <div id="sq-card-number" />
+              <input type="hidden" id="card-nonce" name="nonce" />
+              <div id="sq-expiration-date" />
+              <div id="sq-cvv" />
+            </div>
+            <input id="name" type="text" placeholder="Name" />
+            <div id="sq-postal-code" />
+          </div>
+          <button
+            type="button"
+            className="button-credit-card"
+            onClick={() => paymentForm.requestCardNonce()}
+          >
+            Pay
+          </button>
+        </div>
+        <p id="error" />
+      </div>
+      {/* <Form id="form-container">
         <Form.Label>New Card</Form.Label>
         <Form.Group>
           <Form.Label>Card holder name</Form.Label>
@@ -145,6 +198,8 @@ const PaymentForm = ({ SqPaymentForm }) => {
           </Col>
         </Row>
 
+        <p id="error" />
+
         <Form.Group>
           <Form.Check
             custom
@@ -156,7 +211,17 @@ const PaymentForm = ({ SqPaymentForm }) => {
             name="relationType"
           />
         </Form.Group>
-      </Form>
+
+        <Form.Row className="d-flex justify-content-end">
+          <Button
+            variant="secondary"
+            className="mr-2"
+            onClick={() => paymentForm.requestCardNonce()}
+          >
+            Request
+          </Button>
+        </Form.Row>
+      </Form> */}
     </Container>
   );
 };
--- src/pages/Customer/Dashboard/Cards/Orders/OrderWizard/PayCreditCard/index.js
+++ src/pages/Customer/Dashboard/Cards/Orders/OrderWizard/PayCreditCard/index.js
@@ -1,17 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import PaymentForm from './PaymentForm';
 
 const PayCreditCard = () => {
   const [loaded, setLoaded] = useState(false);
-  const sqPaymentScript = document.createElement('script');
-  sqPaymentScript.src = 'https://js.squareupsandbox.com/v2/paymentform';
-  sqPaymentScript.type = 'text/javascript';
-  sqPaymentScript.async = false;
-  sqPaymentScript.onload = () => {
-    setLoaded(true);
-  };
-  document.getElementsByTagName('head')[0].appendChild(sqPaymentScript);
+
+  useEffect(() => {
+    const sqPaymentScript = document.createElement('script');
+    sqPaymentScript.src = 'https://js.squareupsandbox.com/v2/paymentform';
+    sqPaymentScript.type = 'text/javascript';
+    sqPaymentScript.async = false;
+    sqPaymentScript.onload = () => {
+      setLoaded(true);
+    };
+    document.getElementsByTagName('head')[0].appendChild(sqPaymentScript);
+  }, []);
+
   return <>{loaded && <PaymentForm SqPaymentForm={window.SqPaymentForm} />}</>;
 };
 
