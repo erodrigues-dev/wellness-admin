b730cc4c80a7a95d25900392c85b6fe1b1915376
Luan Panno <luanpanno@gmail.com>
2020-12-07 18:46:57 -0300

    settings retract adjusment

--- src/components/layouts/Sidebar/index.js
+++ src/components/layouts/Sidebar/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { FiLogOut, FiEdit, FiSettings } from 'react-icons/fi';
 import { RiArrowUpSLine } from 'react-icons/ri';
 import { Link, matchPath, useLocation } from 'react-router-dom';
