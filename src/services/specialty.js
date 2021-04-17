228e17a02a7b21351154fec95aa905f1d5ad135c
Elton Rodrigues <erodrigues.dev@gmail.com>
2021-04-17 10:19:21 -0300

    feat: create specialty service

---
+++ src/services/specialty.js
@@ -0,0 +1,35 @@
+import api from './api';
+
+export function list(page, filters) {
+  return api.get('/specialties', {
+    params: {
+      page,
+      limit: 10,
+      ...filters,
+    },
+  });
+}
+
+export function listAll() {
+  return api.get('/specialties');
+}
+
+export function register(data) {
+  return api.post('/specialties', data);
+}
+
+export function update(data) {
+  return api.put('/specialties', data);
+}
+
+export function destroy(id) {
+  return api.delete(`/specialties/${id}`);
+}
+
+export default {
+  list,
+  listAll,
+  register,
+  update,
+  destroy,
+};
