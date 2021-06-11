078d48ffb4d850fcab2d8dd91b3a79519c0d65b5
Elton Rodrigues <erodrigues.dev@gmail.com>
2021-06-11 18:59:17 -0300

    feat: list customer waivers

--- src/pages/CustomerWaiver/index.js
+++ src/pages/CustomerWaiver/index.js
@@ -1,10 +1,72 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Button, Card } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+import { FUNCTIONALITIES } from '~/consts/functionalities';
+import useAuth from '~/contexts/auth';
+import service from '~/services/customerWaiver';
 
 import { List } from './List';
 
-export const CustomerWaiver = () => (
-  <div>
-    <h2>CustomerWaiverPage works!</h2>
-    <List />
-  </div>
-);
+const LIMIT = 10;
+
+export const CustomerWaiver = () => {
+  const { hasPermission } = useAuth();
+
+  const { id: customerId } = useParams();
+  const [data, setData] = useState({
+    list: [],
+    total: 0,
+    page: 1,
+    limit: LIMIT,
+  });
+
+  const allowCreate = hasPermission(FUNCTIONALITIES.settings.waivers.create);
+  const allowEdit = hasPermission(FUNCTIONALITIES.settings.waivers.update);
+  const allowDelete = hasPermission(FUNCTIONALITIES.settings.waivers.delete);
+
+  const fetchList = useCallback(async () => {
+    try {
+      const { data: list, headers } = await service.list(
+        data.page,
+        data.limit,
+        customerId
+      );
+      setData((current) => ({
+        ...current,
+        list,
+        total: Number(headers['x-total-count']),
+      }));
+    } catch (error) {
+      toast.error('Unable to list waivers');
+    }
+  }, [customerId, data.limit, data.page]);
+
+  useEffect(() => {
+    fetchList();
+  }, [fetchList]);
+
+  return (
+    <Card body>
+      <Card.Title>Customer Waivers</Card.Title>
+      <hr />
+      <div className="mt-2 d-flex justify-content-end align-items-start">
+        {allowCreate && (
+          <Button variant="secondary" className="ml-2" onClick={() => {}}>
+            Add Waiver
+          </Button>
+        )}
+      </div>
+      <List
+        data={data}
+        allowEdit={allowEdit}
+        allowDelete={allowDelete}
+        // onDisplay={handleDisplay}
+        // onEdit={handleEdit}
+        // onDelete={handleDelete}
+        onPaginate={(page) => setData((current) => ({ ...current, page }))}
+      />
+    </Card>
+  );
+};
