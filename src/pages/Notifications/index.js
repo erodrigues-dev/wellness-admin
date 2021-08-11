5c2b25b0a5b74e1515e8fe73db458b1009e3dae8
Elton Rodrigues <erodrigues.dev@gmail.com>
2021-08-11 20:59:55 -0300

    feat: create page

---
+++ src/pages/Notifications/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+export function Notifications() {
+  return (
+    <Card body>
+      <Card.Title>Notifications</Card.Title>
+      <hr />
+    </Card>
+  );
+}
