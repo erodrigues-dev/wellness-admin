1199251a74a455d0d243cacc2b733cd70de67e0b
Elton Rodrigues <erodrigues.dev@gmail.com>
2021-09-28 21:26:32 -0300

    fix: parse pageLimit to number

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,6 +1,6 @@
 export const config = Object.freeze({
   apiUrl: process.env.REACT_APP_API_URL,
-  pageLimit: process.env.REACT_APP_PAGE_LIMIT || 10,
+  pageLimit: Number(process.env.REACT_APP_PAGE_LIMIT) || 10,
   squareAppId: process.env.REACT_APP_SQUARE_APP_ID,
   squareLocationId: process.env.REACT_APP_SQUARE_LOCATION_ID,
   socketUrl: process.env.REACT_APP_SOCKET_URL,
