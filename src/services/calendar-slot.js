00f4cbe22d9f0ff0a0a5c92f2279c6a017489364
Elton Rodrigues <erodrigues.dev@gmail.com>
2021-10-18 19:46:46 -0300

    feat: create service

---
+++ src/services/calendar-slot.js
@@ -0,0 +1,36 @@
+import api from './api';
+
+async function list({ calendarId }) {
+  const { data } = await api.get(`/calendars/${calendarId}/slots`);
+
+  return data.map((item) => ({
+    ...item,
+    start: new Date(item.start),
+    end: new Date(item.end),
+    recurrenceExceptions: item.recurrenceExceptions.map(
+      (exceptionDate) => new Date(exceptionDate)
+    ),
+  }));
+}
+
+function store({ calendarId, created, updated, deleted }) {
+  const mapToApi = (item) => ({
+    id: item.id,
+    start: item.start,
+    end: item.end,
+    recurrenceRule: item.recurrenceRule,
+    recurrenceExceptions: item.recurrenceExceptions,
+    status: 'available',
+  });
+
+  return api.post(`/calendars/${calendarId}/slots`, {
+    created: created.map(mapToApi),
+    updated: updated.map(mapToApi),
+    deleted: deleted.map(mapToApi),
+  });
+}
+
+export default {
+  list,
+  store,
+};
