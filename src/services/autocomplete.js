257e04b770c18c64195e256865085ee5c2211590
Elton Rodrigues <erodrigues.dev@gmail.com>
2021-12-06 21:17:14 -0300

    refactor: rename attibute to disableGlobalLoading

--- src/services/autocomplete.js
+++ src/services/autocomplete.js
@@ -3,7 +3,7 @@ import api from './api';
 export const customers = (q) => {
   return api.get('autocomplete/customers', {
     params: { q },
-    disableAutoLoading: true,
+    disableGlobalLoading: true,
   });
 };
 
