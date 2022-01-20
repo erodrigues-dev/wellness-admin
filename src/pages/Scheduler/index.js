86b8c3bde589200f6f06f592d89e83ef4cab9fbe
Elton Rodrigues <erodrigues.dev@gmail.com>
2022-01-19 22:32:26 -0300

    feat: adjust layout and improve list calendars

--- src/pages/Scheduler/index.js
+++ src/pages/Scheduler/index.js
@@ -1,55 +1,101 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { toast } from 'react-toastify';
 
 import { Scheduler, DayView } from '@progress/kendo-react-scheduler';
+import styled from 'styled-components';
 
 import calendarService from '~/services/calendar';
 
-import { group, data } from './data';
-
-const defaultDate = new Date('2022-01-18T00:00');
+import { settings } from './settings';
 
 export function MainScheduler() {
   const [resources, setResources] = useState([]);
+  const contentRef = useRef(null);
 
-  useEffect(() => {
-    calendarService.index({}).then(({ data: list }) => {
-      const categoriesMap = list.reduce((map, calendar) => {
-        return { ...map, [calendar.category.id]: calendar.category };
-      }, {});
-
+  const fetchCalendars = useCallback(async () => {
+    try {
+      const { data } = await calendarService.index({});
       setResources([
         {
           name: 'Calendars',
-          data: list,
+          data,
           field: 'calendarId',
           valueField: 'id',
           textField: 'name',
         },
-        {
-          name: 'Categories',
-          data: Object.values(categoriesMap),
-          field: 'categoryId',
-          valueField: 'id',
-          textField: 'name',
-        },
       ]);
-    });
+    } catch (error) {
+      toast.error('Unable to load calendars');
+    }
   }, []);
 
+  useEffect(() => {
+    fetchCalendars();
+  }, [fetchCalendars]);
+
   return (
-    <div>
+    <Container>
       <h3>Scheduler</h3>
-      <Scheduler
-        group={{
-          resources: ['Calendars'],
-          orientation: 'horizontal',
-        }}
-        resources={resources}
-        data={data}
-        defaultDate={defaultDate}
+      <Content
+        ref={contentRef}
+        contentWidth={contentRef.current?.clientWidth}
+        schedulerWidth={resources[0]?.data.length * settings.calendarMinWidth}
       >
-        <DayView />
-      </Scheduler>
-    </div>
+        <Scheduler
+          height={contentRef.current?.clientHeight - 20 || 600}
+          group={{
+            resources: ['Calendars'],
+            orientation: 'horizontal',
+          }}
+          resources={resources}
+          data={[]}
+        >
+          <DayView
+            startTime={settings.startTime}
+            endTime={settings.endTime}
+            workDayStart={settings.workDayStart}
+            workDayEnd={settings.workDayEnd}
+            slotDivisions={settings.slotDivisions}
+            slotDuration={settings.slotDuration}
+          />
+        </Scheduler>
+      </Content>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Content = styled.div`
+  flex: 1;
+
+  .k-scheduler {
+    width: ${(props) => props.contentWidth}px;
+  }
+
+  // set fixed times column on scroll horizontal
+  .k-scheduler .k-scheduler-day-view .k-side-cell {
+    position: sticky;
+    left: 0;
+    z-index: 2;
+    background: #fff;
+  }
+
+  // set min width for columns
+  .k-scheduler-head,
+  .k-scheduler-body {
+    min-width: ${(props) => props.schedulerWidth}px;
+  }
+
+  // hide allday slot and date slot
+  .k-scheduler-head
+    .k-scheduler-group:nth-child(1)
+    .k-scheduler-row:nth-child(2),
+  .k-scheduler-head .k-scheduler-group:nth-child(2) {
+    display: none;
+  }
+`;
