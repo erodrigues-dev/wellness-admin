b85aacdead8f86f6844c0c5430c35449ea794e3a
Elton Rodrigues <erodrigues.dev@gmail.com>
2022-01-19 22:32:07 -0300

    feat: create settings file for scheduler

---
+++ src/pages/Scheduler/settings.js
@@ -0,0 +1,9 @@
+export const settings = {
+  startTime: '01:00',
+  endTime: '23:00',
+  workDayStart: '07:00',
+  workDayEnd: '19:00',
+  slotDivisions: 4,
+  slotDuration: 60,
+  calendarMinWidth: 260,
+};
