03b85e1d83da5c70f0581938879a1f121fe27b2a
Elton Rodrigues <erodrigues.dev@gmail.com>
2022-02-08 20:15:03 -0300

    feat: hide times dropdown on click in hour or minutes

--- src/components/TimePicker/styles.js
+++ src/components/TimePicker/styles.js
@@ -3,4 +3,8 @@ import styled from 'styled-components';
 export const Container = styled.div`
   position: absolute;
   z-index: 2;
+
+  .react-timekeeper__time-dropdown {
+    display: none;
+  }
 `;
