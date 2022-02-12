f69077764c77e568f18595aed64f2e3067d87a15
Elton Rodrigues <erodrigues.dev@gmail.com>
2022-02-11 21:59:34 -0300

    feat: remove calendar entries service

--- src/services/calendar-entry.js
+++
@@ -1,9 +0,0 @@
-import api from './api';
-
-export async function list({ calendars, date }) {
-  const { data } = await api.get('/calendars/scheduler/entries', {
-    params: { calendars, date },
-  });
-
-  return data;
-}
